4374c49e764c9dbedd925a762996cb6678782067
BlueScrumMU <61719306+BlueScrumMU@users.noreply.github.com>
2020-03-02 20:58:46 -0500

    test update

--- src/canvasproject/CanvasProject.java
+++ src/canvasproject/CanvasProject.java
@@ -28,7 +28,7 @@ public class CanvasProject extends Application {
     }
 
     /**
-     * This is an .
+     * This is  .
      * @param args the command line arguments
      */
     public static void main(String[] args) {
